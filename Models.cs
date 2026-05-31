c96e429b6838d758352e05a29e3071b326ab3f9a
Daniil Kostrykin <daniilkostrykin1@gmail.com>
2026-05-31 13:56:35 +0300

    Add initial implementation of Rooms, Guests, and Health controllers with basic GET endpoints

---
+++ Models.cs
@@ -0,0 +1,4 @@
+namespace HotelApi.Models;
+
+public class Room { public int Id { get; set; } public string Number { get; set; } }
+public class Guest { public int Id { get; set; } public string Name { get; set; } }
